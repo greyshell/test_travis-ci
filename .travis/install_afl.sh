c34bbea3e43a03801c47a7e8f8dbbb8324eaf8f8
Abhijit Sinha <grey.shell@gmail.com>
2020-10-16 11:41:09 -0400

    Push slack.alert.py

--- .travis/install_afl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# author: greyshell
-# description: install the afl fuzzer
-
-wget https://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz && tar xvf afl-latest.tgz
-# shellcheck disable=SC2164
-cd afl*/ && make && sudo make install
-echo "core" | sudo tee /proc/sys/kernel/core_pattern
